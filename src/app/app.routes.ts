dd5c2dd51add1b6297ee7a6b324d76ed9f1e4929
vandersonbfs <vanderson_b@hotmail.com>
2025-06-30 14:37:10 -0300

    Criado rotas no app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
+// Importa o tipo Routes do Angular Router, que define a estrutura das rotas da aplicação
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// Importa o componente que será exibido quando a rota 'painel-principal' for acessada
+import { PainelPrincipal } from './componentes/painel-principal/painel-principal';
+
+// Importa o componente que será exibido quando a rota 'cadastro-produtos' for acessada
+import { CadastroProduto } from './componentes/cadastro-produto/cadastro-produto';
+
+// Define um array de rotas para a aplicação Angular
+export const routes: Routes = [
+    {
+        // Define a rota 'painel-principal'
+        path: 'painel-principal',
+        // Informa qual componente deve ser exibido quando essa rota for acessada
+        component: PainelPrincipal
+    },
+    {
+        // Rota padrão (vazia): será acionada quando o caminho da URL estiver vazio
+        path: '',
+        // Redireciona automaticamente para a rota '/painel-principal'
+        redirectTo: '/painel-principal',
+        // Garante que o redirecionamento só ocorra quando o caminho for exatamente vazio (sem correspondências parciais)
+        pathMatch: 'full'
+    },
+    {
+        // Define a rota 'cadastro-produtos'
+        path: 'cadastro-produtos',
+        // Informa qual componente deve ser exibido quando essa rota for acessada
+        component: CadastroProduto
+    }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,17 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Menu } from "./componentes/menu/menu";
+import { Rodape } from "./componentes/rodape/rodape";
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    HttpClientModule
-  ],
+    HttpClientModule,
+    Menu,
+    Rodape
+],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
