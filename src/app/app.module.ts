5b728a77e718936ee684f1cd38326805ac29a78f
vandersonbfs <vanderson_b@hotmail.com>
2025-07-14 13:13:52 -0300

    teste

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './componentes/menu/menu.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
+import { PainelPrincipalComponent } from './componentes/painel-principal/painel-principal.component';
+import { CadastroProdutoComponent } from './componentes/cadastro-produto/cadastro-produto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    RodapeComponent,
+    PainelPrincipalComponent,
+    CadastroProdutoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/componentes/painel-principal/painel-principal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-painel-principal',
+  templateUrl: './painel-principal.component.html',
+  styleUrls: ['./painel-principal.component.css']
+})
+export class PainelPrincipalComponent {
+
+}
